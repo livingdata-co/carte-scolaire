239b00b9ed26dd53aaf8bd06a479d8aed848a92d
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-02-22 22:50:26 +0400

    feat: create custom server

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 /* eslint import/no-unassigned-import: off */
+/* eslint unicorn/prefer-top-level-await: off */
 import 'dotenv/config.js'
 
 import process from 'node:process'
 import morgan from 'morgan'
 import express from 'express'
 import createError from 'http-errors'
+import next from 'next'
 
 import w from './lib/util/w.js'
 import errorHandler from './lib/util/error-handler.js'
@@ -12,30 +14,38 @@ import {validateCoordinates} from './lib/util/validate.js'
 
 import {search} from './lib/search.js'
 
-const app = express()
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({dev})
+const handle = app.getRequestHandler()
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'))
-}
+app.prepare().then(() => {
+  const server = express()
 
-app.get('/secteur', w(async (req, res) => {
-  const lon = Number.parseFloat(req.query.lon)
-  const lat = Number.parseFloat(req.query.lat)
-
-  const coordinates = validateCoordinates([lon, lat])
-  const college = search(coordinates)
-
-  if (!college) {
-    throw createError(404, 'Territoire non couvert')
+  if (process.env.NODE_ENV !== 'production') {
+    server.use(morgan('dev'))
   }
 
-  res.send(college)
-}))
+  server.get('/secteur', w(async (req, res) => {
+    const lon = Number.parseFloat(req.query.lon)
+    const lat = Number.parseFloat(req.query.lat)
 
-app.use(errorHandler)
+    const coordinates = validateCoordinates([lon, lat])
+    const college = search(coordinates)
 
-const port = process.env.PORT || 5000
+    if (!college) {
+      throw createError(404, 'Territoire non couvert')
+    }
 
-app.listen(port, () => {
-  console.log(`Start listening on port ${port}`)
+    res.send(college)
+  }))
+
+  server.get('*', (req, res) => handle(req, res))
+
+  server.use(errorHandler)
+
+  const port = process.env.PORT || 3000
+
+  server.listen(port, () => {
+    console.log(`Start listening on port ${port}`)
+  })
 })
