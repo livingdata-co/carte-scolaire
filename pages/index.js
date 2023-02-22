d1fbb089d83eff19ae7469791e9692c959e9b142
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-02-22 21:35:54 +0400

    refactor: remove pop up

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
-import {useState, useContext, useMemo} from 'react'
+import {useContext, useMemo} from 'react'
 
 import DeviceContext from '@/contexts/device.js'
 
-import Popup from '@/components/popup.js'
 import Page from '@/layouts/main.js'
 import {Desktop, Mobile} from '@/layouts/map.js'
 
 const Homepage = () => {
-  const [isPopupOpen, setIsPopupOpen] = useState(true)
   const {isMobileDevice} = useContext(DeviceContext)
 
   const Layout = useMemo(() => isMobileDevice ? Mobile : Desktop, [isMobileDevice])
 
   return (
     <Page>
-      {isPopupOpen && <Popup handleClose={() => setIsPopupOpen(false)} />}
       <Layout />
     </Page>
   )
