3c4f4f04e6aeba59b682d70f59ae737b8f948f89
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-02-22 23:02:19 +0400

    refactor: main.js & global.css correction

--- layouts/main.js
+++ layouts/main.js
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types'
 import Header from '@/components/header.js'
 
 const Main = ({children}) => (
-  <div>
+  <>
     <Header />
     <main>{children}</main>
-  </div>
+  </>
 )
 
 Main.propTypes = {
