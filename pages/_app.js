2681ec8aef18bd918f79e25a08c831cf739eeb81
Marine Le Pennec <lepennec.marine@gmail.com>
2023-02-22 13:32:16 +0100

    Init front

---
+++ pages/_app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import '../styles/global.css'
+
+const App = ({Component, pageProps}) => (
+  <React.StrictMode>
+      <Component {...pageProps} />
+  </React.StrictMode>
+)
+
+App.propTypes = {
+  Component: PropTypes.any.isRequired,
+  pageProps: PropTypes.object.isRequired
+}
+
+export default App
