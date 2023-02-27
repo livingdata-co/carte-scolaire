191b99f164b63513c1e1606bda6eeb2fb11d01f8
Jérôme Desboeufs <jerome@livingdata.co>
2023-02-27 23:25:03 +0100

    Serve tiles

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ import process from 'node:process'
 import morgan from 'morgan'
 import express from 'express'
 import createError from 'http-errors'
+import cors from 'cors'
 import next from 'next'
 
 import w from './lib/util/w.js'
 import errorHandler from './lib/util/error-handler.js'
 import {validateCoordinates} from './lib/util/validate.js'
+import {getTile} from './lib/util/mbtiles.js'
 
 import {search} from './lib/search.js'
 
@@ -24,6 +26,24 @@ app.prepare().then(() => {
   if (process.env.NODE_ENV !== 'production') {
     server.use(morgan('dev'))
   }
+
+  server.use(cors({origin: true}))
+
+  server.get('/tiles/:z/:x/:y', w(async (req, res) => {
+    try {
+      const tile = await getTile(req.params.z, req.params.x, req.params.y)
+      res.set('Content-Encoding', 'gzip')
+      res.set('Content-Type', 'application/x-protobuf')
+      res.set(tile.headers)
+      res.send(tile.data)
+    } catch (error) {
+      if (error.message === 'Tile does not exist') {
+        return res.sendStatus(404)
+      }
+
+      throw error
+    }
+  }))
 
   server.get('/secteur', w(async (req, res) => {
     const lon = Number.parseFloat(req.query.lon)
