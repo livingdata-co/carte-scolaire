838863c3f00cac1f39d181d2d4740b7a41b23e92
Jérôme Desboeufs <jerome@livingdata.co>
2023-02-28 22:05:21 +0100

    Remove non visible layers/sources

--- components/map/layers.js
+++ components/map/layers.js
@@ -1,22 +1,2 @@
 export const layers = [
-  {
-    id: 'departements-layer',
-    type: 'line',
-    source: 'decoupage-administratif',
-    'source-layer': 'departements',
-    paint: {
-      'line-color': '#666666',
-      'line-width': 0.1
-    }
-  },
-  {
-    id: 'communes-line',
-    type: 'line',
-    source: 'decoupage-administratif',
-    'source-layer': 'communes',
-    paint: {
-      'line-color': '#D1D3BD',
-      'line-width': 1
-    }
-  }
 ]
--- components/map/sources.js
+++ components/map/sources.js
@@ -1,13 +1,2 @@
 export const sources = [
-  {
-    id: 'decoupage-administratif',
-    options: {
-      type: 'vector',
-      promoteId: 'gid',
-      format: 'pbf',
-      tiles: [
-        'https://openmaptiles.geo.data.gouv.fr/data/decoupage-administratif/{z}/{x}/{y}.pbf'
-      ]
-    }
-  }
 ]
