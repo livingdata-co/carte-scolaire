036f814295ee342378038116b65c02c438aad190
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-03-01 10:05:47 +0400

    feat: remove sidebar button

--- layouts/map.js
+++ layouts/map.js
@@ -1,8 +1,5 @@
-import {useState} from 'react'
 import PropTypes from 'prop-types'
-import {ChevronLeft, ChevronRight} from 'react-feather'
 
-import colors from '@/styles/colors.js'
 import size from '@/styles/size.js'
 
 import Map from '@/components/map/index.js'
@@ -60,75 +57,47 @@ Mobile.defaultProps = {
 }
 
 // Desktop layout
-export const Desktop = ({selectedAdresse, selectedCollege, onSelectAdresse, onSelectCollege}) => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
-
-  return (
-    <div className='desktop-layout-container'>
-      <div className='layout-sidebar-wrapper'>
-        <button
-          className='switch-button'
-          type='button'
-          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-        >
-          {isSidebarOpen ? <ChevronLeft /> : <ChevronRight />}
-        </button>
-        {isSidebarOpen && (
-          <MapSidebar
-            selectedAdresse={selectedAdresse}
-            selectedCollege={selectedCollege}
-            onSelectAdresse={onSelectAdresse}
-            onSelectCollege={onSelectCollege}
-          />
-        )}
-      </div>
-
-      <div className='layout-map-wrapper'>
-        <Map
-          selectedAdresse={selectedAdresse}
-          selectedCollege={selectedCollege}
-        />
-      </div>
-
-      <style jsx>{`
-        .desktop-layout-container {
-          display: flex;
-          contain: content;
-        }
-
-        .layout-sidebar-wrapper {
-          min-width: ${isSidebarOpen ? size.sidebar : '5px'};
-          max-width: ${size.sidebar};
-          box-shadow: 0px 0px 5px grey;
-          height: calc(100vh - ${size.header});
-          z-index: 1;
-          overflow: auto;
-          overflow-x: hidden;
-        }
-
-        .switch-button {
-          position: absolute;
-          top: 25px;
-          left: ${isSidebarOpen ? size.sidebar : '5px'};
-          background-color:${colors.white};
-          height: 50px;
-          width: 30px;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          border-radius: 0 5px 5px 0;
-          box-shadow: 2px 2px 5px grey;
-        }
-
-        .layout-map-wrapper {
-          width: 100%;
-          height: calc(100vh - ${size.header});
-        }
-      `}</style>
+export const Desktop = ({selectedAdresse, selectedCollege, onSelectAdresse, onSelectCollege}) => (
+  <div className='desktop-layout-container'>
+    <div className='layout-sidebar-wrapper'>
+      <MapSidebar
+        selectedAdresse={selectedAdresse}
+        selectedCollege={selectedCollege}
+        onSelectAdresse={onSelectAdresse}
+        onSelectCollege={onSelectCollege}
+      />
     </div>
-  )
-}
+
+    <div className='layout-map-wrapper'>
+      <Map
+        selectedAdresse={selectedAdresse}
+        selectedCollege={selectedCollege}
+      />
+    </div>
+
+    <style jsx>{`
+      .desktop-layout-container {
+        display: flex;
+        contain: content;
+      }
+
+      .layout-sidebar-wrapper {
+        min-width: ${size.sidebar};
+        max-width: ${size.sidebar};
+        box-shadow: 0px 0px 5px grey;
+        height: calc(100vh - ${size.header});
+        z-index: 1;
+        overflow: auto;
+        overflow-x: hidden;
+      }
+
+      .layout-map-wrapper {
+        width: 100%;
+        height: calc(100vh - ${size.header});
+      }
+    `}</style>
+  </div>
+)
 
 Desktop.propTypes = {
   selectedAdresse: PropTypes.object,
