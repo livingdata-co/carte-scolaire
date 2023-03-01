3dbf951bb5f1b6dcd82b48c836e9991fff7c1746
Jérôme Desboeufs <jerome@livingdata.co>
2023-03-01 22:03:20 +0100

    Move download scripts to scripts dir

--- scripts/download-prepared-data.js
+++ scripts/download-prepared-data.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import {downloadTo} from './lib/util/download.js'
+import {downloadTo} from '../lib/util/download.js'
 
 console.log('  * Téléchargement de colleges.geojson')
 
 await downloadTo(
   'https://carte-scolaire-data.s3.fr-par.scw.cloud/colleges.geojson',
-  './dist/colleges.geojson',
+  '../dist/colleges.geojson',
   import.meta.url
 )
 
@@ -13,7 +13,7 @@ console.log('  * Téléchargement de secteurs.db')
 
 await downloadTo(
   'https://carte-scolaire-data.s3.fr-par.scw.cloud/secteurs.db',
-  './secteurs.db',
+  '../secteurs.db',
   import.meta.url
 )
 
@@ -21,6 +21,6 @@ console.log('  * Téléchargement de carte-scolaire.mbtiles')
 
 await downloadTo(
   'https://carte-scolaire-data.s3.fr-par.scw.cloud/carte-scolaire.mbtiles',
-  './carte-scolaire.mbtiles',
+  '../carte-scolaire.mbtiles',
   import.meta.url
 )
--- scripts/download-sources.js
+++ scripts/download-sources.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import {downloadTo} from './lib/util/download.js'
+import {downloadTo} from '../lib/util/download.js'
 
 console.log('  * Téléchargement de carte-scolaire.json')
 
 await downloadTo(
   'https://data.education.gouv.fr/explore/dataset/fr-en-carte-scolaire-colleges-publics/download?format=json&timezone=Europe/Berlin&use_labels_for_header=false',
-  './sources/carte-scolaire.json',
+  '../sources/carte-scolaire.json',
   import.meta.url
 )
 
@@ -13,6 +13,6 @@ console.log('  * Téléchargement de etablissements.json')
 
 await downloadTo(
   'https://data.education.gouv.fr/explore/dataset/fr-en-adresse-et-geolocalisation-etablissements-premier-et-second-degre/download?format=json&timezone=Europe/Berlin',
-  'etablissements.json',
+  '../sources/etablissements.json',
   import.meta.url
 )
