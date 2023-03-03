70edd96556bafa946df30de7289e18c4ddf79c73
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-03-03 14:35:47 +0400

    feat: add layers to source

--- components/map/layers.js
+++ components/map/layers.js
@@ -1,2 +1,50 @@
+import colors from '@/styles/colors.js'
+
 export const layers = [
+  {
+    id: 'secteurs-lines',
+    source: 'secteurs',
+    'source-layer': 'secteurs',
+    type: 'line',
+    paint: {
+      'line-color': colors.darkGrey,
+      'line-width': 1
+    },
+    layout: {
+      visibility: 'none'
+    }
+  },
+  {
+    id: 'secteurs-fill',
+    source: 'secteurs',
+    'source-layer': 'secteurs',
+    type: 'fill',
+    paint: {
+      'fill-color': colors.blue,
+      'fill-opacity': 0.1
+    },
+    layout: {
+      visibility: 'none'
+    }
+  },
+  {
+    id: 'colleges',
+    source: 'secteurs',
+    'source-layer': 'colleges',
+    type: 'circle',
+    paint: {
+      'circle-color': [
+        'case',
+        ['boolean', ['feature-state', 'selected'], false],
+        colors.blue,
+        colors.green
+      ],
+      'circle-radius': 10
+    },
+    filter: [
+      'all',
+      ['==', 'secteur', 'Public'],
+      ['!has', 'erreur']
+    ]
+  }
 ]
