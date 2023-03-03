db60a40033052d9176841c6b86da67e943c65c62
Cédric Famibelle-Pronzola <c.pronzola@tuta.io>
2023-03-03 14:35:04 +0400

    feat: add tiles source

--- components/map/sources.js
+++ components/map/sources.js
@@ -1,2 +1,16 @@
+/* eslint n/prefer-global/process: off */
+
+const TILES_URL = process.env.NEXT_PUBLIC_TILES_URL || 'http://localhost:3000'
+
 export const sources = [
+  {
+    id: 'secteurs',
+    options: {
+      type: 'vector',
+      promoteId: 'codeRNE',
+      tiles: [`${TILES_URL}/tiles/{z}/{x}/{y}`],
+      maxzoom: 12,
+      minzoom: 8
+    }
+  }
 ]
